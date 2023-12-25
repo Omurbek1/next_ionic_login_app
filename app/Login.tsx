f31bfa40068fd549e2f7eca622f5929bf5cb9f99
Omur <omurbekmamytbekov545@gmail.com>
2023-12-25 19:23:42 +0600

    add auth informatons

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,29 +1,42 @@
-"use client"
+"use client";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { UserCard } from "./userCard";
+import "./page.module.css";
 
 export default function Login() {
-    // get session from nextAuth
-    const { data: session } = useSession();
-    console.log(session);
-    // useSession uses React Context
+  // get session from nextAuth
+  const { data: session } = useSession();
+  console.log(session);
+  // useSession uses React Context
 
-    // if the user exists -> show a Sign Out button and their information
-    if(session) {
-        return (
-            <>
-                <button onClick={() => signOut()} type="button" className="btn btn-primary">Sign Out of Google</button>
-                {/* Pass session info to server component */}
-                <UserCard user={session?.user}/>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <button onClick={() => signIn()} type="button" className="btn btn-primary">Sign In with Google</button>
-            </>
-        )
-    }
+  // if the user exists -> show a Sign Out button and their information
+  if (session) {
+    return (
+      <>
+        <button
+          onClick={() => signOut()}
+          type='button'
+          className='btn btn-primary'
+        >
+          Sign Out of Google{" "}
+        </button>
+        {/* Pass session info to server component */}
+        <UserCard user={session?.user} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <button
+          onClick={() => signIn()}
+          type='button'
+          className='btn btn-primary'
+        >
+          Sign In with Google
+        </button>
+      </>
+    );
+  }
 
-    // if a user doesn't exist -> show a Sign In button
+  // if a user doesn't exist -> show a Sign In button
 }
--- app/userCard.tsx
+++ app/userCard.tsx
@@ -1,16 +1,15 @@
 import { DefaultSession } from "next-auth";
-
+import "./page.module.css";
 // Default Session TYPE
 
-export function UserCard( { user } : { user: DefaultSession["user"]}) {
-    
-    return (
-        <div className="card">
-            <div className="card-body">
-                <p>Current Logged In User</p>
-                <h5 className="card-title">{user?.name}</h5>
-                <p className="card-text">{user?.email}</p>
-            </div>
-        </div>
-    )
+export function UserCard({ user }: { user: DefaultSession["user"] }) {
+  return (
+    <div className='card mt-3'>
+      <div className='card-body'>
+        <p>Current Logged In User</p>
+        <h5 className='card-title'>{user?.name}</h5>
+        <p className='card-text'>{user?.email}</p>
+      </div>
+    </div>
+  );
 }
