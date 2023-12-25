ba83f82d7ca83e59d8c3a9c07bed410c1ff3ab04
Omur <omurbekmamytbekov545@gmail.com>
2023-12-25 23:11:27 +0600

    add profile image

--- app/userCard.tsx
+++ app/userCard.tsx
@@ -3,10 +3,12 @@ import "./page.module.css";
 // Default Session TYPE
 
 export function UserCard({ user }: { user: DefaultSession["user"] }) {
+  console.log(user);
   return (
     <div className='card mt-3'>
       <div className='card-body'>
         <p>Current Logged In User</p>
+        <img src={user?.image} alt={user?.name} />
         <h5 className='card-title'>{user?.name}</h5>
         <p className='card-text'>{user?.email}</p>
       </div>
