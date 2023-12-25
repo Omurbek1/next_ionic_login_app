2bd3eec1c9dce3758b82dd703f382a5e5d37a294
Omur <omurbekmamytbekov545@gmail.com>
2023-12-25 23:12:59 +0600

    remove profile avatar

--- app/userCard.tsx
+++ app/userCard.tsx
@@ -8,7 +8,6 @@ export function UserCard({ user }: { user: DefaultSession["user"] }) {
     <div className='card mt-3'>
       <div className='card-body'>
         <p>Current Logged In User</p>
-        <img src={user?.image} alt={user?.name} />
         <h5 className='card-title'>{user?.name}</h5>
         <p className='card-text'>{user?.email}</p>
       </div>
